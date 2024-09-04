b3936ffcaceb0bf7cab6f3f59f6f4c1d0556984b
mohamedafshan <158749915+mohamedafshan@users.noreply.github.com>
2024-09-04 11:20:00 +0530

    php layout setup with out header fixing
    php layout setup with out header fixing

--- index.php
+++ index.php
@@ -1,27 +1,2 @@
-<?php require 'index.view.php' ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-  
-    <!-- Array -->
-     <p>The Array list below</p>
-
-  <ul>
-    <?php foreach($filteredBooks as $book) : ?> 
-        <li>
-              <a href="<?= $book['puchaseUrl'] ?>">
-                  <?= $book['author']; ?> (<?= $book['releaseYear'] ?>) - by <?= $book['author'] ?>
-              </a>
-        </li>
-    <?php endforeach; ?>
-  </ul>
-
-    <!-- <?php echo $student[0]['name'] ?>    -->
-
-</body>
-</html>
+<?php 
+require "index.view.php";
