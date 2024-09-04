d611387151fbf7f09d1ce80d2c5dca446b44dde5
mohamedafshan <158749915+mohamedafshan@users.noreply.github.com>
2024-09-04 12:09:50 +0530

    Full Layout Finished works
    Header , Footer , Static Content part

---
+++ views/contact.view.php
@@ -0,0 +1,18 @@
+ <!-- here one cut html content to past to the header part -->
+ <?php require("views/partials/head.php") ?>
+
+ <!-- nav part -->
+ <?php require("views/partials/nav.php") ?>
+
+ <header class="bg-white shadow">
+     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+         <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact</h1>
+     </div>
+ </header>
+ <main>
+     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+         <!-- Your content -->
+         <p>Hello. Welcome to the contact page.</p>
+     </div>
+ </main>
+ <?php require('views/partials/footer.php') ?>
