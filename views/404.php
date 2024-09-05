809dd6c45e050bdf4797ecb790e4394d57d8b74b
mohamedafshan <158749915+mohamedafshan@users.noreply.github.com>
2024-09-05 11:39:04 +0530

    route concept with out name it route
    route concept with out name it route , then next part is name called route

---
+++ views/404.php
@@ -0,0 +1,18 @@
+ <!-- here one cut html content to past to the header part -->
+ <?php require("views/partials/head.php") ?>
+
+ <!-- nav part -->
+ <?php require("views/partials/nav.php") ?>
+
+ <!-- here no need banner becouse banner need header variable   -->
+
+ <main>
+     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+         <!-- Your content -->
+          <h1 class="text-2xl font-bold">Sorry. Page Not Fount.</h1>
+         <p class="mt-4">
+            <a href="/" class="text-blue underline">Go Back to Home Page</a>
+        </p>
+     </div>
+ </main>
+ <?php require('views/partials/footer.php') ?>
