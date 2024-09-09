48872c235a4009ceb8355c404a57f9fc00c4db12
mohamedafshan <158749915+mohamedafshan@users.noreply.github.com>
2024-09-09 15:06:43 +0530

    PDO Concept
    PDO Concept

--- index.php
+++ index.php
@@ -2,4 +2,18 @@
 //this file actually used get the enpoint of the current uri
 
 require 'function.php';
-require 'router.php';
+// require 'router.php';
+
+$dsn = "mysql:host=localhost;port=3306;dbname=posts;user=root;charset=utf8mb4";
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("SELECT * FROM post");
+$statement->execute();
+
+// $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+// dd($posts);
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+foreach ($posts as $post){
+    echo "<li>".$post['title']."</li>";
+}
