b7986ca89d93c0586f775e4e4344e3a41cc0e5d8
mohamedafshan <158749915+mohamedafshan@users.noreply.github.com>
2024-09-09 15:24:42 +0530

    PDO Database connectivity
    PDO Database connectivity

--- index.php
+++ index.php
@@ -4,16 +4,28 @@
 require 'function.php';
 // require 'router.php';
 
-$dsn = "mysql:host=localhost;port=3306;dbname=posts;user=root;charset=utf8mb4";
-$pdo = new PDO($dsn);
 
-$statement = $pdo->prepare("SELECT * FROM post");
-$statement->execute();
+class Database{
 
-// $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+    public $connection;
+    public function __construct()
+    {
+        $dsn = "mysql:host=localhost;port=3306;dbname=posts;user=root;charset=utf8mb4";
+        $this->connection = new PDO($dsn);
+    }
+
+    public function query($query){
+        $statement = $this->connection->prepare($query); 
+        $statement->execute();
+        return $statement;
+    }
+}
+
+$db =  new Database();
+$posts = $db->query("SELECT * FROM post")->fetch(PDO::FETCH_ASSOC);
+
 // dd($posts);
 
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
 foreach ($posts as $post){
     echo "<li>".$post['title']."</li>";
 }
